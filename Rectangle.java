7cb30445fb8bf5003b37ee1ad72e4d83cb2815db
eshwar23 <enigma.bluewater@gmail.com>
2021-07-25 22:30:33 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -3,7 +3,6 @@ class Rectangle implements Polygon,PolygonPerimeter
     public void Area()
     {
         System.out.println("Area of Rectangle is "+(10*4));
-        
     }
     public void Perimeter()
     {
